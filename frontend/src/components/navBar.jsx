4da3ec5a574a733d094ea01ca92c97eb98b00cd7
Jacksparrow7-coder <sugishiva0@gmail.com>
2026-03-25 17:58:51 +0530

    added font icons for sidenav and nnav

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -1,7 +1,13 @@
+import { FaRegBell } from "react-icons/fa6";
+import { IoSettingsOutline, IoPerson } from "react-icons/io5";
+
 const NavBar = () => {
   return (
-    <div className="flex justify-end px-6 py-3 bg-white border-b">
-      <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+    
+    <div className="flex items-center justify-end px-6 py-3 space-x-3 bg-white border-b">
+      <FaRegBell className="w-8 h-8 p-2 bg-gray-300 rounded-2xl" />
+      <IoSettingsOutline className="w-8 h-8 p-2 bg-gray-300 rounded-2xl" />
+      <IoPerson className="w-8 h-8 p-2 bg-gray-300 rounded-2xl" />
     </div>
   );
 };
--- frontend/src/components/sideBar.jsx
+++ frontend/src/components/sideBar.jsx
@@ -1,13 +1,15 @@
+import { FaMale, FaClipboard, FaCalendar, FaFacebookMessenger } from "react-icons/fa";
+
 const sideBar = () => {
   return (
     <div className="h-screen p-5 bg-white border-r w-60">
       <h2 className="mb-6 font-bold text-blue-600">RS-TECH</h2>
 
       <ul className="space-y-3">
-        <li className="p-2 text-white bg-blue-500 rounded hover:cursor-pointer">Employee</li>
-        <li className="p-2 text-gray-600 hover:cursor-pointer">Dashboard</li>
-        <li className="p-2 text-gray-600 hover:cursor-pointer">Calendar</li>
-        <li className="p-2 text-gray-600 hover:cursor-pointer">Messages</li>
+        <li className="p-2 text-gray-600 rounded hover:cursor-pointer hover:text-white hover:bg-blue-500"><FaMale />Employee</li>
+        <li className="p-2 text-gray-600 rounded hover:cursor-pointer hover:text-white hover:bg-blue-500"><FaClipboard /> Dashboard</li>
+        <li className="p-2 text-gray-600 rounded hover:cursor-pointer hover:text-white hover:bg-blue-500"> <FaCalendar /> Calendar</li>
+        <li className="p-2 text-gray-600 rounded hover:cursor-pointer hover:text-white hover:bg-blue-500"> <FaFacebookMessenger /> Messages</li>
       </ul>
     </div>
   );
